49788a7e555a38f27a2ceab76ed56fee5942f0c5
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-04-05 14:47:09 -0700

    Setup and some light styling for splash page
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

--- spec/models/keynote_spec.rb
+++ spec/models/keynote_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Keynote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :mode }
+  it { should validate_presence_of :keynote }
+  it { should validate_presence_of :image }
+  it { should validate_presence_of :audio }
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :username }
+  it { should validate_uniqueness_of(:username).case_insensitive }
+  it { should validate_presence_of :email }
+  it { should validate_uniqueness_of(:email).case_insensitive }
+  it { should validate_presence_of :password }
+  it { should validate_confirmation_of :password }
 end
