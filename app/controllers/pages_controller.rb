85ba5a0305a900c98c621056590e327871eadd22
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-04-06 16:55:30 -0700

    Fully implement flashcards
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,24 @@ class PagesController < ApplicationController
   end
 
   def cards
-    @selected_keys = params.values[1..-4]
+    # @all_keys = Keynote.all
+    @selected_keys = params.keys[1..-4]
+    @display_keys = []
+    @selected_keys.each do |id|
+      if !id.include?('all')
+        @display_keys.push(Keynote.find(id.to_i))
+      end
+    end
+    if @selected_keys.include?('all_keys')
+      Keynote.all.each { |keynote| @display_keys.push(keynote) }
+    end
+    if @selected_keys.include?('all_major')
+      Keynote.all.each { |keynote| @display_keys.push(keynote) if keynote.mode == "Major" }
+    end
+    if @selected_keys.include?('all_minor')
+      Keynote.all.each { |keynote| @display_keys.push(keynote) if keynote.mode == "minor" }
+    end
+    @display_keys.uniq! {|e| e[:id] }
     render :cards
   end
 end
