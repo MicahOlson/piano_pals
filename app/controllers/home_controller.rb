7543af18791481240fcf3a53a174a368b208f1cf
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-04-05 17:06:51 -0700

    Setup more UI incorporating our table properties
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

--- app/controllers/home_controller.rb
+++
@@ -1,7 +0,0 @@
-class HomeController < ApplicationController
-
-  def index
-    @chords = Keynote.all
-    render :index
-  end
-end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+
+  def home
+    render :home
+  end
+
+  def setup
+    @chords = Keynote.all
+    render :setup
+  end
+
+  def cards
+    @chords = Keynote.find(params[:keynote])
+    @refined_chords = @chords.find(params[:mode])
+    render :cards
+  end
+end
