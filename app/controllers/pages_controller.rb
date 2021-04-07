44363b1ea06c4347349f6d97c5687c5f347e5186
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-04-07 11:14:54 -0700

    Work on styling and add radomizer
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,6 @@ class PagesController < ApplicationController
   end
 
   def cards
-    # @all_keys = Keynote.all
     @selected_keys = params.keys[1..-4]
     @display_keys = []
     @selected_keys.each do |id|
@@ -20,14 +19,15 @@ class PagesController < ApplicationController
     end
     if @selected_keys.include?('all_keys')
       Keynote.all.each { |keynote| @display_keys.push(keynote) }
+    else
+      if @selected_keys.include?('all_major')
+        Keynote.all.each { |keynote| @display_keys.push(keynote) if keynote.mode == "Major" }
+      end
+      if @selected_keys.include?('all_minor')
+        Keynote.all.each { |keynote| @display_keys.push(keynote) if keynote.mode == "minor" }
+      end
     end
-    if @selected_keys.include?('all_major')
-      Keynote.all.each { |keynote| @display_keys.push(keynote) if keynote.mode == "Major" }
-    end
-    if @selected_keys.include?('all_minor')
-      Keynote.all.each { |keynote| @display_keys.push(keynote) if keynote.mode == "minor" }
-    end
-    @display_keys.uniq! {|e| e[:id] }
+      @display_keys.uniq! { |e| e[:id] }
     render :cards
   end
 end
