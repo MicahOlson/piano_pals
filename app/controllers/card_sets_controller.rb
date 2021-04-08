f6964b0907d21815affb14417773c1a131cab175
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-04-08 11:20:45 -0700

    Get card sets to display in carousel with settings
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

--- app/controllers/card_sets_controller.rb
+++ app/controllers/card_sets_controller.rb
@@ -1,39 +1,28 @@
 class CardSetsController < ApplicationController
-  # before_action :only => %i[ new create edit delete ] do
-  #   redirect_to new_user_session_path unless current_user && user_signed_in?
-  # end
 
-  # before_action do :only => [:]
-  #   redirect_to new_user_session_path unless (current_user && card_set.user_id = current_user.id)
-  # end
+  before_action :only => [:edit, :delete] do
+    if @card_set = CardSet.where(id: params[:id]).first
+    unless current_user && @card_set.user_id == current_user.id
+      flash[:notice] = "You are not authorized to visit that page"
+      redirect_to root_path
+    end
+    else
+      flash[:notice] = "The card set with that id does not exist."
+        redirect_to root_path
+    end
+  end
+
 
   def index
     @card_array = []
     @card_sets = CardSet.all
-    @card_sets.each do |card_set|  
-      if card_set.user_id = current_user.id
+    @card_sets.each do |card_set|
+      if card_set.user_id == current_user.id
         @card_array.push(card_set)
       end
-    end 
+    end
     render :index
   end
-
-  # def index
-  #   @display_keys = []
-  #   @card_array = []
-  #   @card_sets = CardSet.all
-  #   @card_sets.each do |card_set|  
-  #     if card_set.user_id = current_user.id
-  #       @card_array.push(card_set)
-  #       card_set.set.split(',').each do |set|
-  #         if set != '0'
-  #           @display_keys.push(Keynote.find(set.to_i))
-  #         end
-  #       end
-  #     end
-  #   end
-  #   render :index
-  # end
 
   def new
     @card_set = CardSet.new
@@ -43,12 +32,10 @@ class CardSetsController < ApplicationController
 
   def create
     set_name = params[:set_name]
-    selected_keys = params.keys[2..-5]
+    selected_keys = params.keys
     display_keys = []
     selected_keys.each do |id|
-      if !id.include?('all')
-        display_keys.push(id.to_i)
-      end
+      display_keys.push(id.to_i)
     end
     if selected_keys.include?('all_keys')
       Keynote.all.each { |keynote| display_keys.push(keynote.id) }
@@ -61,8 +48,13 @@ class CardSetsController < ApplicationController
       end
     end
     display_keys.uniq!
+    display_keys.delete(0)
+    if selected_keys.include?('all_randomize')
+      display_keys.push("T")
+    else
+      display_keys.push("F")
+    end
     set = display_keys.join(",")
-    
     @card_set = CardSet.new(set_name: set_name, set: set, user_id: current_user.id)
     if @card_set.save
       flash[:notice] = "Set successfully added!"
@@ -72,31 +64,34 @@ class CardSetsController < ApplicationController
     end
   end
 
-  # def show
-  #   @card_set = CardSet.find(params[:id])
-  #   id_array = @card_set.set.split(",")
-  #   @keynotes_array = []
-  #   id_array.each do |id|
-  #     @keynotes_array.push(Keynote.find(id.to_i))
-  #   end
-  #   render :show
-  # end
+  def show
+    @selected_keys = []
+    @card_set = CardSet.find(params[:id])
+    @id_array = @card_set.set.split(",")
+    @random = @id_array.pop
+    @display_keys = []
+    @id_array.each do |id|
+      @display_keys.push(Keynote.find(id.to_i))
+    end
+    render :show
+  end
 
   def edit
     @keynotes = Keynote.all
     @card_set = CardSet.find(params[:id])
     @id_array = @card_set.set.split(",")
+    @random = @id_array.pop
+
+
     render :edit
   end
-  
+
   def update
     set_name = params[:set_name]
-    selected_keys = params.keys[2..-5]
+    selected_keys = params.keys
     display_keys = []
     selected_keys.each do |id|
-      if !id.include?('all')
-        display_keys.push(id.to_i)
-      end
+      display_keys.push(id.to_i)
     end
     if selected_keys.include?('all_keys')
       Keynote.all.each { |keynote| display_keys.push(keynote.id) }
@@ -108,9 +103,15 @@ class CardSetsController < ApplicationController
         Keynote.all.each { |keynote| display_keys.push(keynote.id) if keynote.mode == "minor" }
       end
     end
-    display_keys.uniq!
-    set = display_keys.join(",")
 
+    display_keys.uniq!
+    display_keys.delete(0)
+    if selected_keys.include?('all_randomize') 
+      display_keys.push("T")
+    else
+      display_keys.push("F")
+    end
+    set = display_keys.join(",")
     @card_set = CardSet.find(params[:id])
     if @card_set.update(set_name: set_name, set: set)
       flash[:notice] = "Set successfully updated!"
